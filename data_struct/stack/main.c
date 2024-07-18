b3b06638885b46353450d99e0b5ca60a1ce23252
LemsumQing <2725989632@qq.com>
2024-07-19 00:43:35 +0800

    使用指针修复了堆栈无法操作的问题

--- data_struct/stack/main.c
+++ data_struct/stack/main.c
@@ -18,30 +18,30 @@ typedef struct
 }UrlStack;
 
 //初始化堆栈，初始化栈顶
-void InitStack(UrlStack tstack)
+void InitStack(UrlStack *tstack)
 {
-	tstack.top = -1;
+	tstack->top = -1;
 }
 
 //出栈
-void push(UrlStack stack,char *p)
+void push(UrlStack *stack,char *p)
 {
-	if(stack.top >= MAX_STACK_SIZE - 1)
+	if(stack->top >= MAX_STACK_SIZE - 1)
 	{
 		printf("The stack is fulled!\n");
 	}
 	else
 	{
-		stack.top++;
-		strcpy(stack.items[stack.top].url,p);
-		printf("The top is %d\n",stack.top);
+		stack->top++;
+		strcpy(stack->items[stack->top].url,p);
+		printf("The top is %d\n",stack->top);
 	}
 }
 
 
-void pop(UrlStack stack)
+void pop(UrlStack *stack)
 {
-	if(stack.top < 0)
+	if(stack->top < 0)
 	{
 		printf("The stack is empty!\n");
 		return;
@@ -49,9 +49,9 @@ void pop(UrlStack stack)
 	else
 	{
 
-		printf("The URL is %s\n",stack.items[stack.top].url);
-		stack.top--;
-		printf("Top is %d\n",stack.top);
+		printf("The URL is %s\n",stack->items[stack->top].url);
+		stack->top--;
+		printf("Top is %d\n",stack->top);
 	}
 }
 
@@ -59,11 +59,11 @@ UrlStack stack;
 
 int main(void)
 {
-	InitStack(stack);
-	push(stack,"bing.com");
-	push(stack,"goole.com");
-	pop(stack);
-	pop(stack);
-	pop(stack);
+	InitStack(&stack);
+	push(&stack,"bing.com");
+	push(&stack,"goole.com");
+	pop(&stack);
+	pop(&stack);
+	pop(&stack);
 	return 0;
 }
